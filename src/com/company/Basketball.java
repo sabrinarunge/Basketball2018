b9db1b3e5de0b23ab6a2e90a247558aeeacd8362
Louis <lrflrf@sbcglobal.net>
2018-08-09 14:26:28 -0500

    Initial check in

---
+++ src/com/company/Basketball.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Basketball
+{
+    public static void main(String[] args)
+    {
+        playGame();
+    }
+
+    private static void playGame()
+    {
+        System.out.println("Welcome to the arena!");
+    }
+}
